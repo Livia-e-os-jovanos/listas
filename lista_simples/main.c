da34ed9ba76f6554f794810daa2d02f611d9ab30
Geovanni Martins <geovanni.mrts@gmail.com>
2025-10-12 16:19:52 -0300

    feat: adição de arquivos a fila de impressão

--- lista_simples/main.c
+++ lista_simples/main.c
@@ -1,33 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Vou fazer uma fila de impressão
 // essa fila de impressão segue o princípio de FIFO (First In, First Out)
 
+struct no {
+    char arquivo[80];
+    struct no *prox;
+};
+
+void inserirArquivo(struct no *ptrCabeca);
+
 int main() {
     int continuar = 1, escolharUser;
+    struct no *cabeca = NULL;
 
     while (continuar == 1) {
 
         // apresenta as opções do usuário e após isso o usuário escolhe
         printf("Escolha:\n");
-        printf("[1] Iniciar Fila de Impressão;\n");
-        printf("[2] Inserir Arquivo na Fila;\n");
-        printf("[3] Remover Arquivo da Fila;\n"); // por indice ou por nome
-        printf("[4] Exibir Fila de Impressão;\n");
-        printf("[5] Exibir o Tamanho da Fila;\n");
-        printf("[6] Exibir o Próximo Arquivo a ser Impresso;\n");
-        printf("[7] Imprimir arquivo;\n"); // vai imprimir um, assim os outros irão para frente após isso e esse será excluído
+        printf("[1] Inserir Arquivo na Fila de Impressão;\n");
+        printf("[2] Remover Arquivo da Fila;\n"); // por indice ou por nome
+        printf("[3] Exibir Fila de Impressão;\n");
+        printf("[4] Exibir o Tamanho da Fila;\n");
+        printf("[5] Exibir o Próximo Arquivo a ser Impresso;\n");
+        printf("[6] Imprimir arquivo;\n"); // vai imprimir um, assim os outros irão para frente após isso e esse será excluído
         printf("[0] Sair;\n");
         //limpar fila?
         
         scanf("%d", &escolharUser);
         switch (escolharUser) {
         case 1:
+            inserirArquivo(cabeca);
             break;
         
         default:
             printf("Opção Inexistente\n");
             break;
         }
+    }
+}
+
+void inserirArquivo(struct no *ptrCabeca) {
+    // cancela resíduos que podem ter ficado na entrada
+    getchar();
+
+    if (ptrCabeca == NULL) { // significa que a lista ainda não possui arquivos
+        ptrCabeca = malloc(sizeof *ptrCabeca);
+
+        printf("Qual o nome do arquivo (+ extensão)? \n");
+        // lê o arquivo, com permissão para espaços
+        fgets(ptrCabeca->arquivo, sizeof(ptrCabeca->arquivo), stdin);
+        // remove o caracter que simboliza o 'enter' ao final da string
+        ptrCabeca->arquivo[strcspn(ptrCabeca->arquivo, "\n")] = '\0';
+        printf("%s adicionado com sucesso\n", ptrCabeca->arquivo);
+    } else {
+        // armazena o espaço do novo arquivo
+        struct no *novoArquivo = malloc(sizeof *novoArquivo);
+
+        // looping até achar o nó que aponta para NULL como próximo
+        while (ptrCabeca->prox != NULL) {
+            ptrCabeca = ptrCabeca->prox;
+        }
+
+        // quem era o último começa a apontar para o novo;
+        ptrCabeca->prox = novoArquivo;
+        // salva os dado do novo arquivo
+        printf("Qual o nome do arquivo (+ extensão)? \n");
+        fgets(novoArquivo->arquivo, sizeof(novoArquivo->arquivo), stdin);
+        novoArquivo->arquivo[strcspn(novoArquivo->arquivo, "\n")] = '\0';
+        printf("%s adicionado com sucesso\n", novoArquivo->arquivo);
+
+        novoArquivo->prox = NULL;
     }
 }
